99c4d243e23706677e774808fc140a27c25949bb
Tomas <tomaslopes2711@gmail.com>
2025-05-22 12:27:46 +0100

    path do enviroment prod

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OD6E772F.mjs";import"./chunk-2P7XLWKA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-P4H7KRWB.mjs";import"./chunk-2P7XLWKA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://sistemas-e-redes.onrender.com' // URL do teu backend no Render
+};
