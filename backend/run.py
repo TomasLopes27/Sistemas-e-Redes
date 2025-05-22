ec3877dec39e4b4211624ac9e9c1f02707fe35b5
Tomas <tomaslopes2711@gmail.com>
2025-05-22 13:07:48 +0100

    fix

--- backend/run.py
+++ backend/run.py
@@ -5,8 +5,7 @@ from routes.concert_interactions import interaction_bp
 from routes.admin_users import admin_bp
 from routes.user_routes import user_bp
 import os
-
-
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -17,7 +16,10 @@ app.register_blueprint(user_bp)
 app.register_blueprint(admin_bp)
 app.config["SECRET_KEY"] = "asdfe"
 
+CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))  # <- usa a porta que o Render fornece
     app.run(host='0.0.0.0', port=port)        # <- obrigatoriamente 0.0.0.0
+
